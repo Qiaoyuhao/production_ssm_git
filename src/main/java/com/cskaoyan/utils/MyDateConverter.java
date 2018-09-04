6cd763389fcca95c0e6f8255ccb7462e98eb6af5
cxy <cxy@0d40d1f2-d06b-6449-87f1-2ff326f21b07>
2018-09-04 08:16:33 +0000

    cxy提交：处理时间封装的utils包MyDateConverter类
    git-svn-id: https://user-20160201uj/svn/production_ssm/production_ssm/trunk@14 0d40d1f2-d06b-6449-87f1-2ff326f21b07

---
+++ src/main/java/com/cskaoyan/utils/MyDateConverter.java
@@ -0,0 +1,25 @@
+package com.cskaoyan.utils;
+
+
+import org.springframework.core.convert.converter.Converter;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MyDateConverter implements Converter<String,Date> {
+
+    @Override
+    public Date convert(String s){
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH-mm-s");
+
+        Date date = null;
+        try {
+            date = simpleDateFormat.parse(s);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return date;
+    }
+}
